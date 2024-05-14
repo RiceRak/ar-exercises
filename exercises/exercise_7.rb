d20563e063144353084b02894ec0d0eefca80033
Shadrak Lagadi <shadrl19@gmail.com>
2024-05-14 17:01:08 -0600

    Finish assignment

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,52 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+class Employee < ActiveRecord::Base
+  belongs_to :store
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, numericality: { only_integer: true, greater_than_or_equal_to: 40, less_than_or_equal_to: 200 }
+  validates :store, presence: true
+end
+
+class Store < ActiveRecord::Base
+  has_many :employees
+
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validate :must_carry_mens_or_womens_apparel
+
+  private
+
+  def must_carry_mens_or_womens_apparel
+    unless mens_apparel || womens_apparel
+      errors.add(:base, "Stores must carry at least one of the men's or women's apparel")
+    end
+  end
+end
+
+class Employee < ActiveRecord::Base
+  belongs_to :store
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, numericality: { only_integer: true, greater_than_or_equal_to: 40, less_than_or_equal_to: 200 }
+  validates :store, presence: true
+end
+
+
+puts "Please enter a store name:"
+store_name = gets.chomp
+
+
+new_store = Store.create(name: store_name)
+
+
+if new_store.errors.any?
+  new_store.errors.full_messages.each do |message|
+    puts message
+  end
+else
+  puts "Store created successfully!"
+end
