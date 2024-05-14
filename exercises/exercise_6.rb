ebf8fcb9039ad076b86f55734f210b0051a61131
Shadrak Lagadi <shadrl19@gmail.com>
2024-05-14 16:56:55 -0600

    Add more fake employees

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -17,3 +17,9 @@ class Employee < ActiveRecord::Base
   belongs_to :store
 end
 
+@store_burnaby.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store_gastown.employees.create(first_name: "Shad", last_name: "Lagadi", hourly_rate: 180)
+@store_richmond.employees.create(first_name: "John", last_name: "Smith", hourly_rate: 40)
+@store_surrey.employees.create(first_name: "Jane", last_name: "Doe", hourly_rate: 50)
+@store_whistler.employees.create(first_name: "Bob", last_name: "Brown", hourly_rate: 55)
+@store_yaletown.employees.create(first_name: "Don", last_name: "Donnington", hourly_rate: 65)
