aba0df1b2f724e25b3ca968ee9cc4a6c4cdf80b7
Shadrak Lagadi <shadrl19@gmail.com>
2024-05-14 16:03:35 -0600

    Create 3 stores using Active Record's 'create' class method

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,4 +3,14 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
+Store.delete_all
+
 # Your code goes below here ...
+
+store_burnaby = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+
+store_richmond = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+
+store_gastown = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+
+puts Store.count
